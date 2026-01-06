26eac619ade35ae04ee580738f2cd97727cc1266
vladmir0512 <vjgamer2001@gmail.com>
2026-01-06 09:34:12 +0300

    Remove collectionId, use only parentDocumentId for Yonote database

--- yonote_client.py
+++ yonote_client.py
@@ -42,7 +42,6 @@ def add_application_to_yonote(full_name: str, telegram_id: int, age: Optional[st
     # Подготавливаем данные
     data = {
         "parentDocumentId": settings.yonote_database_id,
-        "collectionId": "1196a54b-fe1b-497f-8f63-d87e86f74bf4",
         "title": full_name,
         "values": {
             TELEGRAM_ID_FIELD: telegram_id
